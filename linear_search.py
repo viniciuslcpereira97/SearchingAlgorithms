089766f07e70ba99ae7456ed4880ded15b9c87fe
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-08-14 15:49:46 -0300

    recursive linear search algorithm

--- linear_search.py
+++ linear_search.py
@@ -7,3 +7,12 @@ def search(n, arr):
         if element == n:
             index = arr.index(element)
     return index
+
+def recursive(n, arr):
+    if not arr:
+        return
+    if arr[0] == n:
+        return n
+    else:
+        arr.pop(0)
+        return recursive(n, arr)
