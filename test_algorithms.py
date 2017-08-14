af2cdaee17caa9845e0afc2793784d78ce5de2da
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-08-14 15:45:00 -0300

    main file to test algorithms

---
+++ test_algorithms.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from datetime import datetime
+
+import linear_search as linear
+import binary_search as binary
+
+n = 9
+arr = range(0, 4)
+
+# linear.search(n, arr)
+print binary.recursive(n, arr)
